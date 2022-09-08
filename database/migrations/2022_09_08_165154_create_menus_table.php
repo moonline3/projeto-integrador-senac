779a2318260a9d76a57299ab5edab33eb595d0a5
moonary <gabrielayumifutsukifeliczaki@hotmail.com>
2022-09-08 14:18:24 -0300

    teste do git

--- database/migrations/2022_09_08_165154_create_menus_table.php
+++ database/migrations/2022_09_08_165154_create_menus_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+
         });
     }
 
