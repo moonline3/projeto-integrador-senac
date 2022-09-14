45b93063a7b95a0bf9c2fe639ff26a0447ecb160
moonary <gabrielayumifutsukifeliczaki@hotmail.com>
2022-09-14 15:26:47 -0300

    pagina de funcionarios

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <body>
+    <div class="container" style="margin-top: 200px">
+    <h2>Funcionários</h2>
+
+    <table class="table">
+      <button type="button" class="btn btn-outline-secondary btn-lg"><a href="file:///home/kingdreemur/Downloads/ProjetoGerente/Login(SemNumero).html">Criar Novo</a></button>
+    <thead>
+      <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">CPF</th>
+        <th scope="col">Endereço</th>
+        <th scope="col">Telefone</th>
+        <th scope="col">Login</th>
+        <th scope="col">Edit</th>
+        <th scope="col">Remover</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Amora Aleno</th>
+        <td>02334578</td>
+        <td>rua marechal</td>
+        <td>429113456</td>
+        <td>bistecafofo@gmail</td>
+        <td><button type="button" class="btn btn-link">Editar</td>
+          <td><button type="button" class="btn btn-link">Remover</td>
+      </tr>
+      <tr>
+        <th scope="row">Milo Castello</th>
+        <td>07354589</td>
+        <td>rua das flores</td>
+        <td>42988253</td>
+        <td>MiloMiguel@gmail</td>
+        <td><button type="button" class="btn btn-link">Editar</td>
+          <td><button type="button" class="btn btn-link">Remover</td>
+      </tr>
+      <tr>
+        <th scope="row"></th>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+  </body>
+</html>
