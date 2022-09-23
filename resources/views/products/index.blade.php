2f96e9efe7a72611ef81ace8df863056660d6611
moonary <gabrielayumifutsukifeliczaki@hotmail.com>
2022-09-23 17:17:33 -0300

    página de produto adicionada

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+  <body class=" bg-dark.bg-gradient" style="width: 100vw; height: 100vh; background-image: url('https://img.freepik.com/fotos-gratis/prancheta-de-vista-superior-com-lapis-em-cima-da-mesa_23-2148539063.jpg?w=2000');
+  height: 100vh">
+    <div class="container">
+
+      <h1 style="margin-top: 30px;">Produtos</h1>
+<button type="button" class="btn btn-light">Criar Novo</button>
+    <div style="margin-top: 20px;border-style: solid; border-color: white; border-width: 1px; border-radius: 10px;background-color: white;">
+        <table class="table">
+
+        <thead>
+        <tr>
+            <th scope="col">Produto</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Disponibilidade</th>
+            <th scope="col">Visualizar</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Remover</th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">Batata Frita com Bacon</th>
+            <td>Porção de batata frita com bacon (200g)</td>
+            <td>R$12.00</td>
+            <td>disponível</td>
+            <td><!-- Botão de Visualizar --></td>
+            <td><!-- Botão de Editar --></td>
+            <td><!-- Botão de Remover --></td>
+        </tr>
+        <tr>
+            <th scope="row">Espetinho de Carne</th>
+            <td>Espetinho de carne bovina (100g)</td>
+            <td>R$10.00 </td>
+            <td>disponível</td>
+            <td><!-- Botão de Visualizar --></td>
+            <td><!-- Botão de Editar --></td>
+            <td><!-- Botão de Remover --></td>
+        </tr>
+        <tr>
+            <th scope="row">Pizza de Bacon Frango</th>
+            <td>Pizza de bacon com  frango e mussarela com tomate 8 pedaços</td>
+            <td>R$10.00 </td>
+            <td>disponível</td>
+            <td><!-- Botão de Visualizar --></td>
+            <td><!-- Botão de Editar --></td>
+            <td><!-- Botão de Remover --></td>
+
+        </tr>
+        <th scope="row">Coca-Cola</th>
+        <td>refrigerante coca cola 500</td>
+        <td>R$12.00</td>
+        <td>disponível</td>
+        <td><!-- Botão de Visualizar --></td>
+        <td><!-- Botão de Editar --></td>
+        <td><!-- Botão de Remover --></td>
+
+
+
+
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
+  </body>
+</html>
