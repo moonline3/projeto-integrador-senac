e73d209d717205319626126fdaaddae6418f1431
moonary <gabrielayumifutsukifeliczaki@hotmail.com>
2022-09-29 15:17:51 -0300

    funcionarios e cadstro de funcionarios

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,58 +1,96 @@
 @extends ('layouts.layout')
 @section ('content')
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+  <body>
+    <div class="bg-image"
+         style="background-image: url('https://img.freepik.com/fotos-gratis/prancheta-de-vista-superior-com-lapis-em-cima-da-mesa_23-2148539063.jpg?w=2000');
+                height: 100vh">
 <div class="d-flex justify-content-center">
 
   Checkl!st
 </div>
-<div style="margin-top: 150px;">
+<div style="margin-top: 150px;"
 <br>
 <div class="container mt-4">
   <h1>Cadastro de Funcionário</h1>
   <br>
-  <form class="row g-3" method="POST" action="{{route('user.store')}}">
-    @csrf
+  <form class="row g-3">
     <div class="col-md-6">
       <label for="inputEmail4" class="form-label">Nome</label>
-      <input type="text" class="form-control" name='name' id="inputEmail4">
+      <input type="text" class="form-control" id="inputEmail4">
     </div>
     <div class="col-md-6">
       <label for="inputPassword4" class="form-label">Telefone</label>
-      <input type="text" class="form-control" name='phone' id="inputPassword4">
+      <input type="number" class="form-control" id="inputPassword4">
     </div>
     <div class="col-md-6">
       <label for="inputEmail4" class="form-label">Email</label>
-      <input type="email" class="form-control" name='email' id="inputEmail4">
+      <input type="email" class="form-control" id="inputEmail4">
     </div>
     <div class="col-md-6">
       <label for="inputPassword4" class="form-label">Senha</label>
-      <input type="password" class="form-control" name='password' id="inputPassword4">
+      <input type="password" class="form-control" id="inputPassword4">
     </div>
     <div class="col-12">
       <label for="inputAddress" class="form-label">Endereço</label>
-      <input type="text" class="form-control" name='address' id="inputAddress" placeholder="1234 Rua Jorje Amálio">
+      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Rua Jorje Amálio">
     </div>
     <div class="col-md-6">
       <label for="inputCity" class="form-label">Cidade</label>
-      <input type="text" class="form-control" name='city' id="inputCity">
+      <input type="text" class="form-control" id="inputCity">
     </div>
     <div class="col-md-4">
-      <label for="inputState" class="form-label">Tipo de Usuário</label>
-      <select id="inputState" name='type' class="form-select">
-        <option value='gerente' selected>Gerente</option>
-        <option value='gerente'>Funcionário</option>
+      <label for="inputState" class="form-label">Estado</label>
+      <select id="inputState" class="form-select">
+        <option selected>Escolha...</option>
+          <option>Acre</option>
+          <option>Alagoas</option>
+          <option>Amapá</option>
+          <option>Amazonas</option>
+          <option>Bahia</option>
+          <option>Brasília</option>
+          <option>Ceará</option>
+          <option>Espírito Santo</option>
+          <option>Goiás</option>
+          <option>Maranhão</option>
+          <option>Mato Grosso</option>
+          <option>Mato Grosso do Sul</option>
+          <option>Minas Gerais</option>
+          <option>Pará</option>
+          <option>Paraíba</option>
+          <option>Paraná</option>
+          <option>Pernambuco</option>
+          <option>Piauí</option>
+          <option>Rio de Janeiro</option>
+          <option>Rio Grande do Norte</option>
+          <option>Rio Grande do Sul</option>
+          <option>Rondônia</option>
+          <option>Roraima</option>
+          <option>Santa Catarina</option>
+          <option>São Paulo</option>
+          <option>Sergipe</option>
+          <option>Tocantins</option>
+
 
       </select>
     </div>
     <div class="col-md-2">
-      <label for="inputZip" class="form-label">CPF</label>
-      <input type="text" name='cpf' class="form-control" id="inputZip">
+      <label for="inputZip" class="form-label">CEP</label>
+      <input type="number" class="form-control" id="inputZip">
     </div>
     <div class="col-12">
       <div class="form-check">
         <input class="form-check-input" type="checkbox" id="gridCheck">
         <label class="form-check-label" for="gridCheck">
-          Useless Piece of Space
+          Check me out
         </label>
       </div>
     </div>
@@ -61,5 +99,13 @@
     </div>
   </form>
 </div>
-</div>
+
+
+      </body>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</html>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,13 +1,26 @@
 @extends ('layouts.layout')
 @section ('content')
 
-    <div class="container" style="margin-top:20px">
-    <h2>Funcionários</h2>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-    <table class="table">
-      <a class="btn btn-outline-secondary btn-lg"href="{{route('user.create')}}">Criar Novo</a>
-    <thead>
-      <tr>
+  <body class=" bg-dark.bg-gradient" style="width: 100vw; height: 100vh; background-image: url('https://img.freepik.com/fotos-gratis/prancheta-de-vista-superior-com-lapis-em-cima-da-mesa_23-2148539063.jpg?w=2000');
+  height: 100vh">
+    <div class="container">
+
+      <h1 style="margin-top: 30px;">Funcionários</h1>
+<button type="button" class="btn btn-light">Criar Novo</button>
+    <div style="margin-top: 20px;border-style: solid; border-color: white; border-width: 1px; border-radius: 10px;background-color: white;">
+        <table class="table">
+
+        <thead>
+        <tr>
         <th scope="col">Nome</th>
         <th scope="col">CPF</th>
         <th scope="col">Endereço</th>
@@ -17,31 +30,29 @@
         <th scope="col">Remover</th>
       </tr>
     </thead>
-
     <tbody>
-      @foreach($users as $user)
       <tr>
-        <th scope="row">{{$user->name}}</th>
-        <td>{{$user->cpf}}</td>
-        <td>{{$user->address}}</td>
-        <td>{{$user->phone}}</td>
-        <td>{{$user->email}}</td>
-        <td><a href="{{route('user.edit', $user->id)}}" class="btn btn-link">Editar </a></td>
-        <td>
-            <form action="{{route('user.destroy', $user->id)}}" method="post">
-            @csrf
-            @method('delete')
-            <button type="submit" class="btn btn-link">Remover</button>
-           </form>
-        </td>
-              <a href="{(route('user.edit', $user->id))}"></a>
-
-
-
+        <th scope="row">Amora Aleno</th>
+        <td>02334578</td>
+        <td>rua marechal</td>
+        <td>(42)91134-5658</td>
+        <td>bistecafofo@gmail</td>
+        <td><button type="button" class="btn btn-link">Editar</td>
+          <td><button type="button" class="btn btn-link">Remover</td>
       </tr>
-      @endforeach
+      <tr>
+        <th scope="row">Milo Castello</th>
+        <td>07354589</td>
+        <td>rua das flores</td>
+        <td>(42)98825-3474</td>
+        <td>MiloMiguel@gmail</td>
+        <td><button type="button" class="btn btn-link">Editar</td>
+          <td><button type="button" class="btn btn-link">Remover</td>
+      </tr>
     </tbody>
   </table>
 </div>
+  </body>
+</html>
 @endsection
 <!-- falta arrumar -->
