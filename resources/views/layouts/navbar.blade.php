1ad63fecea7ddb556fc7d0b08dbe9997844ff0cc
moonary <gabrielayumifutsukifeliczaki@hotmail.com>
2022-10-05 14:44:53 -0300

    navbar provisório só

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg bg-light">
+<div class="container-fluid">
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <a class="navbar-brand" href="#">Cardápios</a>
+  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="Login.html">Produtos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Ver Pedidos</a>
+    </li>
+    <li class="nav-item dropdown">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="file:///home/kingdreemur/Downloads/ProjetoGerente/9Funcion%C3%A1rios.html">Funcionários</a>
+    <li class="nav-item">
+      <a class="nav-link disabled">Dados da Empresa</a>
+    </li>
+  </ul>
+</div>
+</div>
+  <div class="d-grid gap-2 d-md-flex justify-content-md-end dropdown">
+<button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+Dropdown button
+</button>
+<ul class="dropdown-menu">
+<li><a class="dropdown-item" href="#">Action</a></li>
+<li><a class="dropdown-item" href="#">Another action</a></li>
+<li><a class="dropdown-item" href="#">Something else here</a></li>
+</ul>
+</div>
+</div>
+</div>
+</nav>
